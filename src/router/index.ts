c72497b285e18c737c188297e7b967da2352469a
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2024-01-21 10:32:54 +0100

    menu et page principales

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,22 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/techno',
+      name: 'techno',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/TechnoView.vue')
+    },
+    {
+      path: '/portofolio',
+      name: 'portofolio',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/PortofolioView.vue')
     }
   ]
 })
